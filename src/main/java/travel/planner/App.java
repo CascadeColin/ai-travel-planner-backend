64852eb87f7fd423cb83e4025889a29ceabea8d3
Colin Marshall <colin.marshall94@gmail.com>
2024-09-26 11:53:54 -0700

    add method to App to bypass CORS issues in front end

--- src/main/java/travel/planner/App.java
+++ src/main/java/travel/planner/App.java
@@ -2,10 +2,28 @@ package travel.planner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    // TODO:  NOT SECURE, will get me through presenting but needs to be fixed later
+    // login gets rejected in front end with a CORS error without this method
+    // presumably I need to add registry mapping for the front end endpoints for login, signup, etc
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET","POST", "PUT", "DELETE", "OPTIONS", "HEAD");
+            }
+        };
     }
 }
